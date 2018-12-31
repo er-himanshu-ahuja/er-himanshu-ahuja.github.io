3bb0303978f40a17866723a779bc3424dcbcc755
Himanshu Ahuja <ahujahimanshu35@gmail.com>
2018-12-31 11:55:56 +0530

    Added Page Loader

---
+++ js/script.js
@@ -0,0 +1,6 @@
+$(window).on('load', function () {
+  "use strict";
+  setTimeout(function () {
+    $("#page-loader").addClass("hide-this").fadeOut(1000, function() { $(this).remove();});
+  }, 600);
+});
