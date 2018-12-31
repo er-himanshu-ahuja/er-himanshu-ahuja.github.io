d5ad7457c15cc5405af77a739e15a46f99b4229e
Himanshu Ahuja <ahujahimanshu35@gmail.com>
2018-12-31 15:21:48 +0530

    Optimized Scripts, and removed jQuery

--- js/script.js
+++ js/script.js
@@ -1,6 +1,29 @@
-$(window).on('load', function () {
+(function () {
   "use strict";
+
+  function fadeOutAndRemove(el) {
+    var fadeTarget = el;
+    var fadeEffect = setInterval(function () {
+      if (!fadeTarget.style.opacity) {
+        fadeTarget.style.opacity = 1;
+      }
+      if (fadeTarget.style.opacity > 0) {
+        fadeTarget.style.opacity -= 0.1;
+      } else {
+        clearInterval(fadeEffect);
+        el.remove();
+      }
+    }, 200);
+  }
+
   setTimeout(function () {
-    $("#page-loader").addClass("hide-this").fadeOut(1000, function() { $(this).remove();});
+    // $("#page-loader").addClass("hide-this").fadeOut(1000, function() { $(this).remove();});
+    var loaderElement = document.getElementById('page-loader');
+    if (loaderElement) {
+      if (loaderElement.classList) {
+        loaderElement.classList.add('hide-this');
+      }
+      fadeOutAndRemove(loaderElement);
+    }
   }, 300);
-});
+})();
